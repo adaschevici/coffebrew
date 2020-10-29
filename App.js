3fd3071764c82edd78660db563e451a315953bee
Artur Daschevici <a.daschevici@gmail.com>
2020-10-29 19:19:04 +0100

    Components :tada:

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+import axios from 'axios'
 
-export default function App() {
+import Constants from 'expo-constants'
+
+const searchUrl = '/businesses/search' // /businesses/search?term=coffee&latitude=37.786882&longitude=-122.399972'
+
+const api = axios.create({
+  baseURL: 'https://api.yelp.com/v3',
+  headers: {
+    Authorization: `Bearer ${Constants.manifest.extra.yelpApiKey}`,
+  },
+})
+
+const ourLocation = {
+  latitude: '37.786882',
+  longitude: '-122.399972',
+}
+
+const oldStreet = {
+  latitude: '51.52653',
+  longitude: '-0.08246',
+}
+
+const App = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get(searchUrl, {
+        params: {
+          categories: 'coffee,coffeeroasteries,coffeeshops',
+          ...oldStreet,
+        },
+      })
+
+      setData(result.data.businesses)
+    }
+    fetchData()
+  }, [])
+  console.log(data)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <ListItem key={item.name} bottomDivider>
+            <Avatar source={{ uri: item.image_url }} />
+            <ListItem.Content>
+              <ListItem.Title>{item.name}</ListItem.Title>
+              <ListItem.Subtitle>{`${item.location.address1}, ${item.location.city}`}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
-});
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+})
+
+export default App
---
+++ app.config.js
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+export default {
+  expo: {
+    name: 'coffeebrew',
+    slug: 'coffeebrew',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    extra: {
+      fact: 'kittens are cool',
+      yelpApiKey: process.env.YELP_API_KEY,
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+}
